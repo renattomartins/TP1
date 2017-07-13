d2d43db97b90bbf0c89d92bc854693d5ddb8ac0d
Renato Martins <renatto.martins@gmail.com>
2017-07-13 11:40:33 -0300

    Adds WCF project as a service layer

---
+++ Estacionamento.Service/EstacionamentoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Estacionamento.Negocio;
+
+namespace Estacionamento.Service
+{
+    public class EstacionamentoService : IEstacionamentoService
+    {
+        public void Checkin(string placa)
+        {
+            Operacoes.Checkin(placa);
+        }
+
+        public double Checkout(string placa)
+        {
+            return Operacoes.Checkout(placa);
+        }
+
+        public int VagasRestantes()
+        {
+            return Operacoes.VagasRestantes();
+        }
+    }
+}
